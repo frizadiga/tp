39487b624a341a1ec7db30ae0bf999e945be96f2
frizadiga <frizadiga@gmail.com>
2019-05-02 00:43:12 +0700

    add 2000 nominal fraction

--- src/components/main-form/formula.js
+++ src/components/main-form/formula.js
@@ -1,6 +1,6 @@
 export const denominator = (input) => {
   let total = input;
-  const nominals = [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50];
+  const nominals = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100, 50];
   const result = [];
 
   for (let i = 0, len = nominals.length; i < len; i += 1) {
