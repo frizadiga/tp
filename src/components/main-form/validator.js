3385b9b1bea2577830e62d8759cf73b44456478a
frizadiga <frizadiga@gmail.com>
2019-05-01 21:10:53 +0700

    handle locale postfix

--- src/components/main-form/validator.js
+++ src/components/main-form/validator.js
@@ -1,6 +1,7 @@
 export default (str) => {
   let error = '';
   str = str.trim();
+  str = str.replace(/,00$/, '');
 
   if (!error && /[\d]+[, ]+[\d]+/.test(str)) error = 'invalid separator';
   if (!error && /[\d ]+Rp\.?/i.test(str)) error = 'valid character in wrong position';
